33d3c6d31c50efd02c7ce0a9a17e33e26f64c5bb
cyberkru <74045103+cyberkru@users.noreply.github.com>
2021-11-30 16:40:44 +0700

    Update scan.sh

--- spiderscan/scan/scan.sh
+++ spiderscan/scan/scan.sh
@@ -12,8 +12,10 @@ SESS=`curl -s --fail "$PROXY_URL/JSON/core/action/clearExcludedFromProxy"`
 
 echo "Ready for testing"
 
-#set maximum spider to 100s
-MAX=`curl "$PROXY_URL/JSON/spider/action/setOptionMaxDuration/?Integer=100"`
+#set maximum spider parameters
+MAX=`curl "$PROXY_URL/JSON/spider/action/setOptionMaxDuration/?Integer=10"`
+MAX_DEPTH=`curl -X GET "$PROXY_URL/JSON/spider/action/setOptionMaxDepth/?Integer=5"`
+MAX_CHILD=`curl -X GET "$PROXY_URL/JSON/spider/action/setOptionMaxChildren/?Integer=10"`
 
 echo "scanning..."
 SCANID=`curl "$PROXY_URL/JSON/spider/action/scan/?url=$TARGET_URL&contextName=&recurse="`
@@ -24,8 +26,14 @@ STATUS='"0"'
 while [ "$STATUS" != '"100"' ]
 do
 STATUS=`curl --fail "$PROXY_URL/JSON/spider/view/status/?scanId=$SCANID" 2> /dev/null | jq '.status'`
-sleep 1
+sleep 2
 echo "current status: $STATUS"
+
+if [ -z "$STATUS" ]
+then
+ exit 1
+fi
+
 done
 
 echo "generating report..."
@@ -34,7 +42,7 @@ sleep 5
 #report
 #curl --fail http://localhost:8090/OTHER/core/other/jsonreport/?formMethod=GET > report.json
 result=`curl --fail "$PROXY_URL/OTHER/core/other/xmlreport/?formMethod=GET" > output/report.xml`
-html=`curl --fail "$PROXY_URL/OTHER/core/other/htmlreport/?formMethod=GET" > output/report.html`
+#html=`curl --fail "$PROXY_URL/OTHER/core/other/htmlreport/?formMethod=GET" > output/report.html`
 
 PRODID=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token ${DOJOKEY}" --url "http://{$DOJOIP}/api/v2/products/?limit=1000" | jq -c '[.results[] | select(.name | contains('\"${PRODNAME}\"'))][0] | .id')
 EGID=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token $DOJOKEY" --url "http://${DOJOIP}/api/v2/engagements/?limit=1000" | jq -c "[.results[] | select(.product == ${PRODID})][0] | .id")
