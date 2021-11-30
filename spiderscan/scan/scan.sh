bb73349e1e0ca90006adc1759a3ec69930ddf019
cyberkru <74045103+cyberkru@users.noreply.github.com>
2021-11-30 17:33:25 +0700

    Update scan.sh

--- spiderscan/scan/scan.sh
+++ spiderscan/scan/scan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+start=$(date +%s)
 echo "URL zap: $PROXY_URL"
 echo "DOJOIP: $DOJOIP"
 echo "Target: $TARGET_URL"
@@ -47,3 +48,7 @@ result=`curl --fail "$PROXY_URL/OTHER/core/other/xmlreport/?formMethod=GET" > ou
 PRODID=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token ${DOJOKEY}" --url "http://{$DOJOIP}/api/v2/products/?limit=1000" | jq -c '[.results[] | select(.name | contains('\"${PRODNAME}\"'))][0] | .id')
 EGID=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token $DOJOKEY" --url "http://${DOJOIP}/api/v2/engagements/?limit=1000" | jq -c "[.results[] | select(.product == ${PRODID})][0] | .id")
 curl -X POST --header "Content-Type:multipart/form-data" --header "Authorization:Token $DOJOKEY" -F "engagement=${EGID}" -F "scan_type=ZAP Scan" -F 'file=@./output/report.xml' --url "http://${DOJOIP}/api/v2/import-scan/"
+
+end=$(date +%s)
+echo "Scanning completed###################"
+echo "Elapsed Time: $(($end-$start)) seconds"
