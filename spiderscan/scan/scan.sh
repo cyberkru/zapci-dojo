188fa25f76ff03371885006e34fdce6b6c6006ca
cyberkru <74045103+cyberkru@users.noreply.github.com>
2022-01-24 18:01:46 +0700

    Update scan.sh

--- spiderscan/scan/scan.sh
+++ spiderscan/scan/scan.sh
@@ -47,7 +47,7 @@ result=`curl --fail "$PROXY_URL/OTHER/core/other/xmlreport/?formMethod=GET" > ou
 
 PRODID=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token ${DOJOKEY}" --url "${DOJOURL}/api/v2/products/?limit=1000" | jq -c '[.results[] | select(.name | contains('\"${PRODNAME}\"'))][0] | .id')
 EGID=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token $DOJOKEY" --url "${DOJOURL}/api/v2/engagements/?limit=1000" | jq -c "[.results[] | select(.product == ${PRODID})][0] | .id")
-curl -X POST --header "Content-Type:multipart/form-data" --header "Authorization:Token $DOJOKEY" -F "engagement=${EGID}" -F "scan_type=ZAP Scan" -F 'file=@./output/report.xml' --url "${DOJOURL}/api/v2/import-scan/"
+curl -X POST --header "Content-Type:multipart/form-data" --header "Authorization:Token $DOJOKEY" -F "engagement=${EGID}" -F "close_old_findings=true" -F "scan_type=ZAP Scan" -F 'file=@./output/report.xml' --url "${DOJOURL}/api/v2/import-scan/"
 
 end=$(date +%s)
 echo "Scanning completed###################"
